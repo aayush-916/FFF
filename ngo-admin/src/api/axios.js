9cc9ac82202bebf77209f747d0c69190b6d5a99d
aayush-916 <aayush91620@gmail.com>
2026-03-21 09:35:42 +0000

    test2

--- ngo-admin/src/api/axios.js
+++ ngo-admin/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://api.aanyasolutions.com/api/v1',
-  withCredentials: true, // Crucial for sending/receiving HttpOnly cookies
+  withCredentials: true, //    Crucial for sending/receiving HttpOnly cookies
 });
 
 export default api;
