27b79ba432bf5618cfaab131261e5ffc5fffe9e9
aayush-916 <aayush91620@gmail.com>
2026-03-21 09:25:04 +0000

    change fff admin api

--- ngo-admin/src/api/axios.js
+++ ngo-admin/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'https://api.aanyasolutions.com/api/v1',
   withCredentials: true, // Crucial for sending/receiving HttpOnly cookies
 });
 
